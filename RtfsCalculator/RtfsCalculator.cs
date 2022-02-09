97299659909623136f9fd6e8e5ec1a03a250ce27
Justin Otayco <jotayco@irthsolutions.com>
2022-02-09 14:49:50 -0500

    gracefully exit ctrl+c

--- RtfsCalculator/RtfsCalculator.cs
+++ RtfsCalculator/RtfsCalculator.cs
@@ -7,6 +7,7 @@ namespace RtfsCalculator
     public class RtfsCalculator
     {
         private readonly IRtfsCalculatorService _rtfsCalculatorService;
+        private bool _continue = true;
         public RtfsCalculator(IRtfsCalculatorService rtfsCalculatorService)
         {
             _rtfsCalculatorService = rtfsCalculatorService;
@@ -14,7 +15,13 @@ namespace RtfsCalculator
         
         public async Task Run()
         {
-            while (true)
+            Console.CancelKeyPress += delegate (object sender, ConsoleCancelEventArgs e)
+            {
+                e.Cancel = true;
+                _continue = false;
+            };
+                
+            while (_continue)
             {
                 Console.Write("Enter formatted string to calculate: ");
                 var input = Console.ReadLine();
